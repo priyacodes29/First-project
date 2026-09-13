073630be109b04663db5457cde2c081becb8a7ad
Priyabaghel22 <swetabaghel159>
2026-09-13 14:11:08 +0530

    add files

--- add.py
+++
@@ -1,6 +0,0 @@
-def number():
-    add = 0
-    for i in range(1,5):
-        add = add + i
-    return(add)
-print(number())
---
+++ user.py
@@ -0,0 +1,7 @@
+number =[]
+
+for i in range(8):
+    num = int(input("enter the number"))
+    number.append(num*2)
+
+    print(number)
